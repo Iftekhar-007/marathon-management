6a931bd6fc6b400d59331a3ad6c81f5e9f195f7b
Iftekhar-007 <shawonsp534@gmail.com>
2025-06-17 20:40:20 +0600

    lottie

--- src/Components/MarathonCard.jsx
+++ src/Components/MarathonCard.jsx
@@ -1,4 +1,9 @@
 import React from "react";
+import locationLottie from "../assets/location.json";
+import run from "../assets/running.json";
+import rightArrow from "../assets/right.json";
+import calendarLot from "../assets/calendar.json";
+import Lottie from "lottie-react";
 
 const MarathonCard = ({ marathon }) => {
   return (
@@ -12,10 +17,52 @@ const MarathonCard = ({ marathon }) => {
           />
         </figure>
         <div className="card-body">
-          <h2 className="card-title">{marathon.title}</h2>
-          <p>{marathon.description}</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
+          <div className="flex justify-start items-center gap-2.5">
+            <h2 className="card-title">{marathon.title}</h2>
+            <Lottie className="w-[44px]" animationData={run}></Lottie>
+          </div>
+          <div className="flex justify-start items-start gap-2.5">
+            <p>
+              <b>Description: </b>
+              {marathon.description}
+            </p>
+          </div>
+          <div className="flex justify-start items-center gap-2">
+            <Lottie
+              className="w-[20px]"
+              animationData={locationLottie}
+              loop={true}
+            />
+            <p>{marathon.location}</p>
+          </div>
+
+          <div className="flex justify-start items-center gap-2.5">
+            <Lottie
+              className="w-[20px]"
+              animationData={calendarLot}
+              loop={true}
+            />
+            <p>
+              <b>Registration Start : </b>
+              {marathon.startRegistrationDate}
+            </p>
+          </div>
+
+          <div className="flex justify-start items-center gap-2.5">
+            <Lottie
+              className="w-[20px]"
+              animationData={calendarLot}
+              loop={true}
+            />
+            <p>
+              <b>Registration End : </b>
+              {marathon.endRegistrationDate}
+            </p>
+          </div>
+          <div className="card-actions justify-start">
+            <button className="btn border-0 bg-transparent hover:bg-transparent hover:border-0 hover:shadow-none text-red-400">
+              View Details...
+            </button>
           </div>
         </div>
       </div>
